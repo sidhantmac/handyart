32d7cbdc9d7e4cd2bfea469614a60581eac96ba6
Sidhant <sidhant.madkaikar@gmail.com>
2022-01-31 20:43:14 +0530

    created registration activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.value
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun registerUser(v: View?) {
+        val myIntent = Intent(this, RegistrationActivity::class.java)
+        startActivity(myIntent)
+    }
+
+    fun loginUser(v: View?) {
+        val myIntent = Intent(this, RegistrationActivity::class.java)
+        startActivity(myIntent)
     }
 }
